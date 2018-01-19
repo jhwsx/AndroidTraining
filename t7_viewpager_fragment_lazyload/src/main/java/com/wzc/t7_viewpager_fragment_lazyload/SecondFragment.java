3bcd869eb2b43efd81cfc007016cfeb3c24d033e
wangzhichao <392337950@qq.com>
2018-01-19 21:20:55 +0800

    1,提交t7,Fragment结合ViewPager之懒加载的例子.

---
+++ t7_viewpager_fragment_lazyload/src/main/java/com/wzc/t7_viewpager_fragment_lazyload/SecondFragment.java
@@ -0,0 +1,122 @@
+package com.wzc.t7_viewpager_fragment_lazyload;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by wzc on 2018/1/18.
+ */
+
+public class SecondFragment extends BaseLazyFragment {
+    private static final String TAG = SecondFragment.class.getSimpleName();
+    private View mView;
+    private TextView mTextView;
+
+    @Override
+    public void onAttach(Context context) {
+        Log.e(TAG, "onAttach");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.e(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        Log.e(TAG, "onCreateView");
+//        return inflater.inflate(R.layout.fragment_second,container,false);
+//    }
+
+    @Override
+    protected void initPrepare() {
+
+    }
+
+    @Override
+    protected void onInvisible() {
+
+    }
+
+    @Override
+    protected void initData() {
+        Log.d(TAG, "数据已加载");
+        mTextView.setText(TAG + "数据已加载");
+    }
+
+    @Override
+    protected View initView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_second, container, false);
+        mTextView = (TextView) mView.findViewById(R.id.textView);
+        return mView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.e(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.e(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.e(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.e(TAG, "onDetach");
+        super.onDetach();
+    }
+
+    @Override
+    public boolean getUserVisibleHint() {
+        boolean userVisibleHint = super.getUserVisibleHint();
+        Log.e(TAG, "getUserVisibleHint: " + userVisibleHint);
+        return userVisibleHint;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.e(TAG, "setUserVisibleHint: " + isVisibleToUser);
+    }
+}
