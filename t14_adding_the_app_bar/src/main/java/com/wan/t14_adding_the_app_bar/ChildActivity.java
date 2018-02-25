734519fb63d30f369e016f4b6bbfda8ae2770dfc
wangzhichao <wangzhichao@adups.com>
2018-02-25 20:01:08 +0800

    1,提交t14,Toolbar

---
+++ t14_adding_the_app_bar/src/main/java/com/wan/t14_adding_the_app_bar/ChildActivity.java
@@ -0,0 +1,30 @@
+package com.wan.t14_adding_the_app_bar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * @author wzc
+ * @date 2018/2/25
+ */
+public class ChildActivity extends AppCompatActivity {
+    public static void start(Context context) {
+        Intent starter = new Intent(context, ChildActivity.class);
+        context.startActivity(starter);
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
---
+++ t14_adding_the_app_bar/src/main/java/com/wan/t14_adding_the_app_bar/MainActivity.java
@@ -0,0 +1,69 @@
+package com.wan.t14_adding_the_app_bar;
+
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+            getSupportActionBar().setIcon(R.drawable.ic_circle_check);
+        }
+
+         findViewById (R.id.btn).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 ChildActivity.start(MainActivity.this);
+             }
+         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Log.d("MainActivity", "onMenuItemActionExpand");
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Log.d("MainActivity", "onMenuItemActionCollapse");
+                return true;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
