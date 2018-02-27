4098d80a745ecea9564611172e4c1bc4bf8426f0
wangzhichao <wangzhichao@adups.com>
2018-02-27 22:15:22 +0800

    1,提交t16,创建自定义view,文档上的PieChart例子

---
+++ t16_creating_custom_views/src/main/java/com/wan/t16_creating_custom_views/MainActivity.java
@@ -0,0 +1,33 @@
+package com.wan.t16_creating_custom_views;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.wan.t16_creating_custom_views.view.PieChart;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Resources res = getResources();
+        final PieChart pie = (PieChart) this.findViewById(R.id.Pie);
+        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
+        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
+        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
+        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
+        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
+        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
+
+        ((Button) findViewById(R.id.Reset)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pie.setCurrentItem(0);
+            }
+        });
+    }
+}
